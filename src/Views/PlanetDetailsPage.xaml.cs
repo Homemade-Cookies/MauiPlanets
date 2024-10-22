46b6ef00ae7a25b43b0297d36d6f29bc4e0dce5a
Homemade-Cookies <cam.kochenower@gmail.com>
2024-10-22 13:27:35 -0500

    Add image zoom

--- src/Views/PlanetDetailsPage.xaml.cs
+++ src/Views/PlanetDetailsPage.xaml.cs
@@ -1,16 +1,66 @@
-﻿namespace MauiPlanets.Views;
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace MauiPlanets.Views;
 
 public sealed partial class PlanetDetailsPage : ContentPage
 {
-	public PlanetDetailsPage(Planet planet)
-	{
-		InitializeComponent();
+    public PlanetDetailsPage(Planet planet)
+    {
+        InitializeComponent();
 
-		this.BindingContext = planet;
+        this.BindingContext = planet;
     }
 
     async void BackButton_Clicked(object sender, EventArgs e)
     {
-		await Navigation.PopAsync();
+        await Navigation.PopAsync();
+    }
+
+    private async void OnImageTapped(object sender, EventArgs e)
+    {
+        if (sender is Image image)
+        {
+            // Create a new page to display the zoomed image
+            var zoomPage = new ContentPage
+            {
+                Content = new Border
+                {
+                    Content = new ScrollView
+                    {
+                        Content = new Image
+                        {
+                            Source = image.Source,
+                            Aspect = Aspect.AspectFit,
+                            GestureRecognizers =
+                            {
+                                new TapGestureRecognizer
+                                {
+                                    Command = new Command(async () =>
+                                    {
+                                        await Navigation.PopAsync();
+                                    })
+                                }
+                            }
+                        }
+                    },
+                    StrokeShape = new RoundRectangle
+                    {
+                        CornerRadius = new CornerRadius(10)
+                    },
+                    Shadow = new Shadow
+                    {
+                        Brush = Brush.Black,
+                        Offset = new Point(10, 10),
+                        Radius = 20,
+                        Opacity = 0.5f
+                    },
+                    BackgroundColor = Colors.White,
+                    Margin = new Thickness(20)
+                }
+            };
+
+            // Navigate to the zoom page
+            await Navigation.PushAsync(zoomPage);
+        }
     }
 }
